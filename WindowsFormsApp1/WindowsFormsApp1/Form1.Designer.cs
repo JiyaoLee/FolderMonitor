66d87ce14bf60ba9dbd7f2070327941431141019
Jiyao.li <jiyaolee@outlook.com>
2020-05-13 21:44:15 +0800

    Add init file

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Edi文件路径";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(551, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TextBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private FileSystemWatcher watcher = new FileSystemWatcher();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetAccess("Users", Application.StartupPath);
+            //watcher.Created += Watcher_Created;
+            //watcher.Error += Watcher_Error;
+
+        }
+
+        private void Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            Watcher(folderBrowserDialog1.SelectedPath, "*.*");
+        }
+
+        private void Watcher(string filepath, string fileFilter)
+        {
+            ClearAllEvents(watcher, "Created");
+            watcher.Path = filepath;
+            watcher.Filter = fileFilter;
+            watcher.EnableRaisingEvents = true;
+            watcher.NotifyFilter = (NotifyFilters.FileName);
+            watcher.IncludeSubdirectories = true;
+        }
+        /// <summary>
+        /// 为指定用户组，授权目录指定完全访问权限
+        /// </summary>
+        /// <param name="user">用户组，如Users</param>
+        /// <param name="folder">实际的目录</param>
+        /// <returns></returns>
+        private static bool SetAccess(string user, string folder)
+        {
+            //定义为完全控制的权限
+            const FileSystemRights Rights = FileSystemRights.FullControl;
+
+            //添加访问规则到实际目录
+            var AccessRule = new FileSystemAccessRule(user, Rights,
+                InheritanceFlags.None,
+                PropagationFlags.NoPropagateInherit,
+                AccessControlType.Allow);
+
+            var Info = new DirectoryInfo(folder);
+            var Security = Info.GetAccessControl(AccessControlSections.Access);
+
+            bool Result;
+            Security.ModifyAccessRule(AccessControlModification.Set, AccessRule, out Result);
+            if (!Result) return false;
+
+            //总是允许再目录上进行对象继承
+            const InheritanceFlags iFlags = InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit;
+
+            //为继承关系添加访问规则
+            AccessRule = new FileSystemAccessRule(user, Rights,
+                iFlags,
+                PropagationFlags.InheritOnly,
+                AccessControlType.Allow);
+
+            Security.ModifyAccessRule(AccessControlModification.Add, AccessRule, out Result);
+            if (!Result) return false;
+
+            Info.SetAccessControl(Security);
+
+            return true;
+        }
+        int count = 0;
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string value = "";
+            count++;
+            Trace.WriteLine(count);
+            try
+            {
+                using (FileStream stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(stream))
+                    {
+                        value = streamReader.ReadToEnd().Replace('\r', '\n');
+                    }
+                }
+                using (StreamWriter streamWriter = new StreamWriter(File.Open(e.FullPath, FileMode.Truncate)))
+                {
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        streamWriter.Write(value);
+                    }
+                    streamWriter.Flush();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(e.FullPath);
+            }
+        }
+
+        public static void ClearAllEvents(object objectHasEvents, string eventName)
+        {
+            if (objectHasEvents != null)
+            {
+                try
+                {
+                    EventInfo[] events = objectHasEvents.GetType().GetEvents(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                    if (events != null && events.Length >= 1)
+                    {
+                        int num = 0;
+                        EventInfo eventInfo;
+                        while (true)
+                        {
+                            if (num >= events.Length)
+                            {
+                                return;
+                            }
+                            eventInfo = events[num];
+                            if (eventInfo.Name == eventName)
+                            {
+                                break;
+                            }
+                            num++;
+                        }
+                        FieldInfo field = eventInfo.DeclaringType.GetField(eventName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                        if (field != null)
+                        {
+                            field.SetValue(objectHasEvents, null);
+                        }
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+        private void TextBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            folderBrowserDialog1.Description = "请选择EDI文件所在文件夹";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                {
+                    MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                //Watcher(folderBrowserDialog1.SelectedPath, "*.*");
+                IFolderMonitor folderMonitor = new FolderMonitor(folderBrowserDialog1.SelectedPath);
+                folderMonitor.CreateEvent += (path) =>
+                {
+                    Thread.Sleep(1000);
+                    count++;
+                    Trace.WriteLine(count);
+                    string value = "";
+                    using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(stream))
+                        {
+                            value = streamReader.ReadToEnd().Replace('\r', '\n');
+                        }
+                        stream.Close();
+                    }
+                    using (StreamWriter streamWriter = new StreamWriter(File.Open(path, FileMode.Truncate)))
+                    {
+                        if (!string.IsNullOrEmpty(value))
+                        {
+                            streamWriter.Write(value);
+                        }
+                        streamWriter.Flush();
+                    }
+                };
+                folderMonitor.Start();
+            }
+        }
+    }
+
+    public interface IFolderMonitor
+    {
+        event Action<string> CreateEvent;
+        void Start();
+    }
+    public class FolderMonitor : IFolderMonitor
+    {
+        private readonly FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+        private readonly Dictionary<string, DateTime> createPendingEvent = new Dictionary<string, DateTime>();
+        private readonly Dictionary<string, DateTime> deletePendingEvent = new Dictionary<string, DateTime>();
+        private readonly System.Threading.Timer change_timer;
+        private bool m_timer_started = false;
+
+        private readonly System.Threading.Timer del_timer;
+        private bool m_flag_del_started = false;
+
+        public event Action<string> CreateEvent;
+
+        public event Action<string> DeleteEvent;
+
+        public FolderMonitor(string dirPath)
+        {
+            fileSystemWatcher.Path = dirPath;
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.Created += new FileSystemEventHandler(OnChange);
+            fileSystemWatcher.Changed += new FileSystemEventHandler(OnChange);
+            change_timer = new System.Threading.Timer(OnTimeOut, null, Timeout.Infinite, Timeout.Infinite);
+            del_timer = new System.Threading.Timer(OnDelTimeOut, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void OnDelTimeOut(object state)
+        {
+            string[] delPaths;
+            lock (deletePendingEvent)
+            {
+                delPaths = deletePendingEvent.Select(m => m.Key).ToArray();
+                for (int i = 0; i < delPaths.Length; i++)
+                {
+                    deletePendingEvent.Remove(delPaths[i]);
+                }
+                if (deletePendingEvent.Count == 0)
+                {
+                    m_flag_del_started = false;
+                    del_timer.Change(Timeout.Infinite, Timeout.Infinite);
+                }
+            }
+            if (DeleteEvent != null)
+            {
+                for (int i = 0; i < delPaths.Length; i++)
+                {
+                    DeleteEvent(delPaths[i]);
+                }
+            }
+        }
+
+        private void OnChange(object sender, FileSystemEventArgs e)
+        {
+            lock (createPendingEvent)
+            {
+                createPendingEvent[e.FullPath] = DateTime.Now;
+                if (!m_timer_started)
+                {
+                    change_timer.Change(1000, 100);
+                    m_timer_started = true;
+                }
+                Trace.WriteLine(createPendingEvent.Count);
+            }
+        }
+        private void OnDelete(object sender, FileSystemEventArgs e)
+        {
+            lock (deletePendingEvent)
+            {
+                deletePendingEvent[e.FullPath] = DateTime.Now;
+                if (!m_flag_del_started)
+                {
+                    del_timer.Change(1000, 100);
+                    m_flag_del_started = true;
+                }
+            }
+        }
+        public void Start()
+        {
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnTimeOut(object state)
+        {
+            List<string> paths;
+            lock (createPendingEvent)
+            {
+                paths = FindReadyPaths(createPendingEvent);
+
+                paths.ForEach(m =>
+                {
+                    createPendingEvent.Remove(m);
+                });
+
+                if (createPendingEvent.Count == 0)
+                {
+                    change_timer.Change(Timeout.Infinite, Timeout.Infinite);
+                    m_timer_started = false;
+                }
+            }
+            if (CreateEvent != null)
+            {
+                paths.ForEach(path =>
+                {
+                    CreateEvent(path);
+                });
+            }
+        }
+
+        private List<string> FindReadyPaths(Dictionary<string, DateTime> events)
+        {
+            List<string> results = new List<string>();
+            DateTime now = DateTime.Now;
+
+            foreach (KeyValuePair<string, DateTime> entry in events)
+            {
+                // If the path has not received a new event in the last 75ms
+                // an event for the path should be fired
+                double diff = now.Subtract(entry.Value).TotalMilliseconds;
+                if (diff >= 75)
+                {
+                    results.Add(entry.Key);
+                }
+            }
+            return results;
+        }
+    }
+}
